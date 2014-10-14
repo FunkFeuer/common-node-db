4e34f2c713a63643a3657b2e7a7ca8ef5e16dce6
Christian Tanzer <tanzer@swing.co.at>
2014-10-14 11:30:52 +0200

    Change license to BSD 3-Clause License
    Change license of all files from the GNU Affero General Public License
    to the BSD 3-clause license.

--- _CNDB/_JNJ/html/dashboard/app.js
+++ _CNDB/_JNJ/html/dashboard/app.js
@@ -1,9 +1,8 @@
 // Copyright (C) 2014 Mag. Christian Tanzer All rights reserved
 // Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
 // #*** <License> ************************************************************#
-// This module is licensed under the terms of either the
-// MIT License or the GNU Affero General Public License (AGPL) Version 3.
-// http://www.c-tanzer.at/license/mit_or_agpl.html
+// This module is licensed under the terms of the BSD 3-Clause License
+// <http://www.c-tanzer.at/license/bsd_3c.html>.
 // #*** </License> ***********************************************************#
 //
 //++
--- _CNDB/__init__.py
+++ _CNDB/__init__.py
@@ -3,19 +3,8 @@
 # Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
 # ****************************************************************************
 #
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Library General Public
-# License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Library General Public License for more details.
-#
-# You should have received a copy of the GNU Library General Public
-# License along with this library; if not, write to the Free
-# Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# This module is licensed under the terms of the BSD 3-Clause License
+# <http://www.c-tanzer.at/license/bsd_3c.html>.
 # ****************************************************************************
 #
 #++
