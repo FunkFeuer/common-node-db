2e3a41111f604cf2f4f22a7c9370bb3f753e3e88
Christian Tanzer <tanzer@swing.co.at>
2015-03-06 10:14:58 +0100

    Fix `lib_dir`
    * Must override, not combine with, inherited value!

--- _CNDB/_GTW/deploy.py
+++ _CNDB/_GTW/deploy.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2014 Mag. Christian Tanzer All rights reserved
+# Copyright (C) 2014-2015 Mag. Christian Tanzer All rights reserved
 # Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
 # #*** <License> ************************************************************#
 # This module is part of the package CNDB.GTW.
-# 
+#
 # This module is licensed under the terms of the BSD 3-Clause License
 # <http://www.c-tanzer.at/license/bsd_3c.html>.
 # #*** </License> ***********************************************************#
@@ -19,6 +19,7 @@
 #    10-Jul-2014 (CT) Creation
 #     2-Sep-2014 (CT) Redefine `Babel` to augment `_package_dirs`
 #     2-Sep-2014 (CT) Add `template_package_dirs` to `_defaults`
+#     6-Mar-2015 (CT) Use `update_combined.List_DC` for `lib_dir`
 #    ««revision-date»»···
 #--
 
@@ -28,6 +29,7 @@ from   __future__ import absolute_import, unicode_literals
 from   _CNDB                    import CNDB
 from   _GTW                     import GTW
 from   _TFL                     import TFL
+from   _TFL.update_combined     import update_combined
 
 import _CNDB._GTW
 import _GTW.deploy
@@ -41,7 +43,8 @@ class _CNDB_deploy_Command_ (_Ancestor) :
     _rn_prefix     = "_CNDB_"
 
     _defaults      = dict \
-        ( lib_dir               = ["cndb", "tapyr"]
+        ( lib_dir  = update_combined.List_DC (["cndb", "tapyr"])
+                         ### override, i.e., don't combine, `lib_dir`
         , template_package_dirs = ["_CNDB._JNJ", "_JNJ"]
         )
 
