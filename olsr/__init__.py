a9d2f5988b35176cc0a41f98d163d563d3421c7b
Ralf Schlatterbeck <rsc@runtux.com>
2018-04-15 13:05:53 +0200

    Remove spider and olsr
    .. they're in their own packages now.

--- olsr/__init__.py
+++
@@ -1,6 +0,0 @@
-# #*** <License> ************************************************************#
-# This module is part of the repository CNDB.
-# 
-# This module is licensed under the terms of the BSD 3-Clause License
-# <http://www.c-tanzer.at/license/bsd_3c.html>.
-# #*** </License> ***********************************************************#
